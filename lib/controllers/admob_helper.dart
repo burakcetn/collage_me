075ba5aef528a174a02bfdeeca8f82f1ae91ac4f
burakcetn <burakcetn@gmail.com>
2023-07-03 20:34:56 +0300

    admob için eklentilere başlandı

---
+++ lib/controllers/admob_helper.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+
+// TODO:Uygulamanın gerçek reklam kodları en son düzelt
+/*
+banner: ca-app-pub-3239992079253104/9306487993
+yerel: ca-app-pub-3239992079253104/6297181278
+geçiş: ca-app-pub-3239992079253104/2357936260
+*/
